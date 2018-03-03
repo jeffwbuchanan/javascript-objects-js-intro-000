f048ea3efe5587a554ba33bb7b672d32895f90ca
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-03-03 21:31:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,6 +11,6 @@ function updatePlaylist (obj, key, value){
 updatePlaylist (playlist, "Slowdive", "Alison");
 
 function removeFromPlaylist (obj, key, value){
-  delete playlist.Slowdive;
+  delete ojb.key;
   return Object.assign (obj, {[key]: value})
 }
