f79d0ea0f28262eb5bfbe7b879ab08016db43f54
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-03-03 21:03:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
-var playList = {
+var playlist = {
   Adele: "Hello",
   Elevation: "There is a Cloud",
   Ed_Sheeran: "Perfect"
 };
 function updatePlaylist (obj, key, value){
-  return Object.assign({}, playList, {Slowdive: ["My Bloody Valentine"]})
+  return Object.assign({}, playlist, {Slowdive: ["My Bloody Valentine"]})
 };
