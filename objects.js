3719109a425ed7542b3c28cc105ef8b6223439ff
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-03-03 20:54:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playList = {
   Ed_Sheeran: "Perfect"
 };
 function updatePlaylist (obj, key, value){
-  return Object.assign({}, obj, {[key]:value})
+  return Object.assign({}, obj, {[key]: value})
 };
 updatePlaylist (playList, "Slowdive", "My Bloody Valentine")
-return playList;
+
